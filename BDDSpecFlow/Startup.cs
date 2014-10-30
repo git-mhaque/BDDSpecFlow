b777237f299711882a7c9c4a177a96cc5f071c82
Mahfuzul Haque <mahfuzul.haque@gmail.com>
2014-10-31 10:45:47 +1100

    Initial commit

---
+++ BDDSpecFlow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BDDSpecFlow.Startup))]
+namespace BDDSpecFlow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
